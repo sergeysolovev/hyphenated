ecd03a3fd0108295d1371995a57473eb1939f9f2
Sergey Solovev <me@sergeysolovev.com>
2019-05-13 19:58:17 +0800

    Patterns json to js (#8)
    * Replace json modules with js for patterns
    
    * Update rollup configurations
    
    * Remove rollup-plugin-json dependency

---
+++ packages/hyphenated-en-us/src/exceptions.js
@@ -0,0 +1,16 @@
+export default [
+  'as-so-ciate',
+  'as-so-ciates',
+  'dec-li-na-tion',
+  'oblig-a-tory',
+  'phil-an-thropic',
+  'present',
+  'presents',
+  'project',
+  'projects',
+  'reci-procity',
+  're-cog-ni-zance',
+  'ref-or-ma-tion',
+  'ret-ri-bu-tion',
+  'ta-ble'
+];
